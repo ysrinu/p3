fc55440e2177855ff328dc1096ff66ede9f791f4
Srinivas Yelamanchili <y_srinu@yahoo.com>
2017-10-24 20:44:27 -0400

    added parameter passing from controller to view. Validation not working

--- app/Http/Controllers/CostController.php
+++ app/Http/Controllers/CostController.php
@@ -11,13 +11,59 @@ class CostController extends Controller
         return view('cost.welcome');
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        return view('cost.index');
+        # Validate the request data
+    $this->validate($request, [
+        'custName' => 'required|alpha',
+        //'shipType' => 'required',
+        //'fromZipCode' => 'required|regex:/\b\d{5}\b/',
+        //'toZipCode' => 'required|regex:/\b\d{5}\b/'
+    ]);
+
+        $custName = $request->input('custName', null);
+        $shipType = $request->input('shipType', null);
+        $fromZipCode = $request->input('fromZipCode', null);
+        $toZipCode = $request->input('toZipCode', null);
+
+        return view('cost.index')->with([
+        'custName' => $custName,
+        'shipType' => $shipType,
+        'booksOnly' => $request->has('booksOnly'),
+        'fromZipCode' => $fromZipCode,
+        'toZipCode' => $toZipCode
+    ]);
     }
 
     public function result($shipType)
     {
         return view('cost.result')->with(['shipType' => $shipType]);;
     }
+
+    public function printRequest(Request $request)
+    {
+
+    # ======== Temporary code to explore $request ==========
+
+    # See all the properties and methods available in the $request object
+    dump($request);
+
+    # See just the form data from the $request object
+    dump($request->all());
+
+    # See just the form data for a specific input, in this case a text input
+    dump($request->input('searchTerm'));
+
+    # See what the form data looks like for a checkbox
+    dump($request->input('caseSensitive'));
+
+    # Boolean to see if the request contains data for a particular field
+    dump($request->has('searchTerm')); # Should be true
+    dump($request->has('publishedYear')); # There's no publishedYear input, so this should be false
+
+    # You can get more information about a request than just the data of the form, for example...
+    dump($request->fullUrl());
+    dump($request->method());
+    dump($request->isMethod('post'));
+   }
 }
--- resources/views/cost/index.blade.php
+++ resources/views/cost/index.blade.php
@@ -11,25 +11,47 @@
      Calculate shipping cost between two zip codes.
  </p>
  </section>
- <form action="/cost/air" method="GET">
+ <form action="/cost" method="GET">
      <fieldset>
          <legend>Customer and Package Information</legend>
          <label for="custName" class="category">Name (Required)</label><br />
-         <input type="text" name="custName" required id="custName" value="" placeholder="Enter Customer Name" autofocus="autofocus"/><br /><br />
-         <label class="category">Shipping Type (Required)</label><br />
-         <input type="radio" name="shipType" required id="ground" value="ground" /><label for="ground">Ground</label><br>
-         <input type="radio" name="shipType" required id="express" value="express" /><label for="express">Express</label><br>
-         <input type="radio" name="shipType" required id="overnight" value="overnight" /><label for="overnight">Overnight</label><br>
+         <input type="text" name="custName" required id="custName" value='{{ $custName }}' placeholder="Enter Customer Name" autofocus="autofocus"/><br />
+         @if($errors->get('custName'))
+            <ul>
+                @foreach($errors->get('custName') as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+         @endif
          <br />
-         <input type="checkbox" name="booksOnly" id="booksOnly" /><label for="booksOnly">Books only</label><br />
+         <label class="category">Shipping Type (Required)</label><br />
+         <input type="radio" name="shipType" required id="ground" value="ground" {{ ($shipType === 'ground') ? 'CHECKED' : '' }} /><label for="ground">Ground</label><br>
+         <input type="radio" name="shipType" required id="express" value="express" {{ ($shipType === 'express') ? 'CHECKED' : '' }}/><label for="express">Express</label><br>
+         <input type="radio" name="shipType" required id="overnight" value="overnight" {{ ($shipType === 'overnight') ? 'CHECKED' : '' }}/><label for="overnight">Overnight</label><br>
+         @if($errors->get('shipType'))
+            <ul>
+                @foreach($errors->get('shipType') as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+         @endif
+         <br />
+         <input type="checkbox" name="booksOnly" id="booksOnly" {{ ($booksOnly) ? 'CHECKED' : '' }} /><label for="booksOnly">Books only</label><br />
      </fieldset><br />
      <fieldset>
          <legend>Shipping Origin and Destination</legend>
          <label for="fromZipCode" class="category">From ZIP Code (Required)</label><br />
-         <input type="text" pattern="[0-9]{5}" required name="fromZipCode" id="fromZipCode" placeholder="Enter 5-digit origin zip code" title="Enter 5-digit zipcode"/><br /><br />
+         <input type="text" pattern="[0-9]{5}" required name="fromZipCode" id="fromZipCode" value="{{ $fromZipCode }}" placeholder="Enter 5-digit origin zip code" title="Enter 5-digit zipcode"/><br /><br />
          <label for="toZipCode" class="category">To ZIP Code (Required)</label><br />
-         <input type="text" pattern="[0-9]{5}" required name="toZipCode" id="toZipCode" placeholder="Enter 5-digit destination zip code" title="Enter 5-digit zipcode"/><br /><br />
+         <input type="text" pattern="[0-9]{5}" required name="toZipCode" id="toZipCode"  value="{{ $toZipCode }}" placeholder="Enter 5-digit destination zip code" title="Enter 5-digit zipcode"/><br /><br />
      </fieldset><br />
      <input id="submitButton" type="submit" name="submitButton" value="Calculate Cost"/><br /><br />
+     @if(count($errors) > 0)
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
  </form>
 @endsection
